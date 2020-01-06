8d0c3548b50682b50c696ccc4226dcbc119454da
GaryHDA <ghare2081@gmail.com>
2020-01-06 15:23:49 -0800

    creating project repo

---
+++ code/sbfngrb.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+import socket
+
+s = socket.socket()
+
+s.connect(("192.168.6.1", 22))
+
+answer = s.recv(1024)
+
+print (answer)
+
+s.close
---
+++ code/tcp_server.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+import socket
+
+TCP_IP = ["192.168.1.87"]
+TCP_PORT = 22
+BUFFER_SIZE = 100
+def bob(x,TCP_PORT):
+    print(x)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print(type(s))
+    s.bind((x, TCP_PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connection address: ', addr )
+    while 1:
+        data=conn.recv(BUFFER_SIZE)
+        if data:
+            print("Received data: ", data)
+            conn.send(data)  #echo
+        else:
+            break
+    conn.close
+
