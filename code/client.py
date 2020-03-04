1eb82a660c0397c84f4d84b0ed5ca48d7aa441a2
GaryHDA <ghare2081@gmail.com>
2020-03-03 18:01:49 -0800

    updated

--- code/client.py
+++ code/client.py
@@ -1,8 +1,9 @@
+
 #! usr/bin/python3
 
 import socket
 """
-This is the script for asking for data from server.  
+This is the script for asking for data from server.
 """
 def clientfunction(host, port):
   s = socket.socket()
@@ -11,5 +12,3 @@ def clientfunction(host, port):
   s.connect((host, port))
   print(s.recv(4096))
   s.close()
-  
- 
